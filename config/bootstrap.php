1c74d23afa73d0ae6ca62ae5ccb00b3ec5db5825
Pavel E. Dedkov <pavel.dedkov@gmail.com>
2017-10-30 15:53:35 +0300

    default config

---
+++ config/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// браузер по умолчанию
+Configure::write('Browser.agent', 'Browser Agent');
+
+// глобальные настройки конфигуратора
+Configure::write('Browser.Curl.Configurator.disabled', ['ip', 'proxy']);
+
+Configure::write('Browser.debug', false);
